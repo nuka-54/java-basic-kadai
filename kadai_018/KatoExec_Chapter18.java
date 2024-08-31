1af76343d840608d7c0c80c038e1b1c4e0ab9134
nuka54 <164641022+nuka-54@users.noreply.github.com>
2024-08-31 12:08:07 +0900

    kadai_018_提出用

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,14 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+	public static void main(String[] args) {
+		Kato_Chapter18 katoTaro = new KatoTaro_Chapter18();
+		Kato_Chapter18 katoIchro = new KatoIchiro_Chapter18();
+		Kato_Chapter18 katoHanako = new KatoHanako_Chapter18();
+		
+		katoTaro.execIntroduce();
+		katoIchro.execIntroduce();
+		katoHanako.execIntroduce();
+	}
+
+}
---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,26 @@
+package kadai_018;
+
+abstract public class Kato_Chapter18 {
+	
+	public String familyName = "加藤";
+	
+	public String givenName;
+	
+	public String address = "東京都中野区○×";
+	
+    public Kato_Chapter18(String givenName) {
+        this.givenName = givenName;
+    }
+	
+	public void commonIntroduce() {
+		System.out.println("名前は" + familyName  + givenName + "です");
+		System.out.println("住所は" + address + "です");
+	}
+
+	abstract void eachIntroduce();
+	
+	public void execIntroduce() {
+		commonIntroduce();
+		eachIntroduce();
+	}
+}
